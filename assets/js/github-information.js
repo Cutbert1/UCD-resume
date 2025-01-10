dec7f9eebbbf57c41815a2952cd64803ac2ecc82
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2025-01-10 10:36:39 +0000

    Add Promise and error handling to github-information.js file

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -4,9 +4,28 @@ function fetchGitHubInformation(event) {
       $("#gh-user-data").html(`<h2>Please enter Github username</h2>`);
       return;
    }
+   //displaying our loader
    $("#gh-user-data").html(
       `<div id = "loader">
           <img src = "assets/css/loader.gif" alt = "loading..."/>
       </div>`)
-
+   
+   //Issue Promise
+   $.when(
+      $.getJSON(`https://api.github.com/users/${username}`)
+   ).then(
+      function(response) {
+         var userData = response
+         $("#gh-user-data").html(userInformationHTML(userData));
+      }, function(errorResponse) {
+         if (errorResponse.status === 404) {
+            $("#gh-user-data").html(
+               `<h2>No info found for user ${username}</h2>`)
+         } else {
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+               `<h2>Error: ${errorResponse.response.JSON.message}</h2>`)
+         }
+      });
+   
 }
