99dcd2bca7558fd1a48a8481e76d487bd6c98706
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2025-01-11 16:56:34 +0000

    Add send email and finalised email integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,21 @@
-console.log("Hello!");
+function sendMail(contactForm) {
+    emailjs.send("service_ilry935","template_977a20n", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value,
+
+    })
+
+    //method for our promise
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+
+    );
+    return false;  // To block from loading a new page
+
+}
